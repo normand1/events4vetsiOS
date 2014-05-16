e5b89dbb228158f5e0cfdc7e2d4db4e1ee3f432f
David Norman <david.norman.w@gmail.com>
2014-05-15 23:56:46 -0700

    Initial Commit

---
+++ Events4Vets/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Events4Vets
+//
+//  Created by davinorm on 5/15/14.
+//  Copyright (c) 2014 David Norman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Events4VetsTests/Events4VetsTests.m
@@ -0,0 +1,34 @@
+//
+//  Events4VetsTests.m
+//  Events4VetsTests
+//
+//  Created by davinorm on 5/15/14.
+//  Copyright (c) 2014 David Norman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Events4VetsTests : XCTestCase
+
+@end
+
+@implementation Events4VetsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
